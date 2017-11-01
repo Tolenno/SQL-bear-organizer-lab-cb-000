15641c5e9031803cc9d9bc65e1521c8a47f514da
Matt Hough <matt@hough.tv>
2017-11-01 22:47:28 +1000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Yogi", 32, "M", "Brown", "Shy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 20, "M", "Black", "Hungry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Jack", 5, "M", "Brown", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Leanne", 2, "F", "Black", "Loud", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Arethra", 5, "F", "Brown", "Playful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Ursula", 31, "F", "Black", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Dame", 49, "F", "Brown", "Shy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Penelope", 11, "F", "Brown", "Busy", 1);
